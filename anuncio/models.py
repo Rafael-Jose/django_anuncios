06898d08e699503594d3e8304745b0b279a52f37
Your Name <you@example.com>
2021-03-23 20:50:25 -0300

    criado model de categoria

--- anuncio/models.py
+++ anuncio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=40)
+
+
+    def __str__(self):
+        return self.titulo
+
+
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=40)
+    preco = models.DecimalField(max_digits=11, decimal_places=2)
